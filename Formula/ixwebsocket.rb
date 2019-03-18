03637f421926b48116acce573403b5122a918ece
Benjamin Sergeant <bsergeant@mz.com>
2019-03-18 15:50:41 -0700

    update to 1.3.0

--- Formula/ixwebsocket.rb
+++ Formula/ixwebsocket.rb
@@ -1,8 +1,9 @@
 class Ixwebsocket < Formula
   desc "WebSocket client and server, and HTTP client command-line tool"
   homepage "https://github.com/machinezone/IXWebSocket"
-  url "https://github.com/machinezone/IXWebSocket/archive/v1.1.0.tar.gz"
-  sha256 "52592ce3d0a67ad0f90ac9e8a458f61724175d95a01a38d1bad3fcdc5c7b6666"
+
+  url "https://github.com/machinezone/IXWebSocket/archive/v1.3.0.tar.gz"
+  sha256 "25c6f9de76d46dff4b152821adffd2c85c56f94e216de6f154e8168b5192de9a"
   depends_on "cmake" => :build
 
   def install
